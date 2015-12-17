503d49e638b2eb2f71e905815b27275893f9aa91
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-17 15:52:30 +0100

    Initial Commit

---
+++ SOExtensions/SOExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SOExtensions.h
+//  SOExtensions
+//
+//  Created by Sebastian Osiński on 17.12.2015.
+//  Copyright © 2015 Sebastian Osiński. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOExtensions.
+FOUNDATION_EXPORT double SOExtensionsVersionNumber;
+
+//! Project version string for SOExtensions.
+FOUNDATION_EXPORT const unsigned char SOExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOExtensions/PublicHeader.h>
+
+
